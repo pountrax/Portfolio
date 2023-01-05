18b308b5be79c06f5d093d56df4072c9fa5f9255
pountrax <pountrax@gmail.com>
2023-01-05 16:00:21 -0300

    PortafolioYA

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Inicio/>}/>
+        <Route path="/PortFolio" element={<Inicio/>}/>
        </Routes>
     </>
   )
