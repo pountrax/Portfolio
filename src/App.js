16c36e11840d77a7f40576682013dc28885a5d3a
pountrax <pountrax@gmail.com>
2023-01-05 14:29:15 -0300

    Portafolio v-2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Comida from './components/Comida';
+
 import './components/styles.css';
 import {Routes,Route} from 'react-router-dom'
 import RecetasInfo from './components/RecetasInfo';
-import Perfil from './components/NavBar/NavBar';
-import NavBar from './components/NavBar/NavBar';
+// import Perfil from './components/NavBar/NavBar';
+// import NavBar from './components/NavBar/NavBar';
 import Inicio from './Paginas/Inicio/Inicio';
 function App() {
   return (
