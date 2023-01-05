db375080c68011d8800dff3b85541fb63b7087cd
pountrax <pountrax@gmail.com>
2023-01-05 15:27:26 -0300

    Portafolio v-4

--- src/components/Redes/Redes.jsx
+++ src/components/Redes/Redes.jsx
@@ -7,7 +7,7 @@ import facebook from '../../img/facebook.png'
 export const Redes = () => {
   return (
     <div className="Redes">
-    <img src={www} className="Iconos" alt="pagina"/><b>Sitio web</b><a>enproceso.com</a>
+    <img src={www} className="Iconos" alt="pagina"/><b>Sitio web</b><a target="_blank"  rel="noopener noreferrer" href="hhttps://pountrax.github.io/portfolio">inicio</a>
     <img src={github} className="Iconos" alt="pagina"/><b>GitHub</b><a target="_blank"  rel="noopener noreferrer" href="https://github.com/pountrax">pountrax</a>
     <img src={instagram} className="Iconos" alt="pagina"/><b>Instagram</b><a target="_blank"  rel="noopener noreferrer" href="https://www.instagram.com/sal.marco_/">sal.marco_</a>
     <img src={facebook} className="Iconos" alt="pagina"/><b>Facebook</b><a target="_blank"  rel="noopener noreferrer" href="https://www.facebook.com/profile.php?id=100003292156200">Marcos Lopez</a>
