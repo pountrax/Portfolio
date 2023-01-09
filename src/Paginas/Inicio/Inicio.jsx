07304695680fbca6e459aa6759db344395615076
pountrax <pountrax@gmail.com>
2023-01-09 15:16:29 -0300

    portfolio-v6

--- src/Paginas/Inicio/Inicio.jsx
+++ src/Paginas/Inicio/Inicio.jsx
@@ -5,15 +5,10 @@ import { Habilidades } from '../../components/Habilidades/Habilidades';
 import { HabilidadesDos } from '../../components/Habilidades/HabilidadesDos';
 import NavBar from '../../components/NavBar/NavBar';
 import { Redes } from '../../components/Redes/Redes';
-import pantalla from '../../img/pantalla.gif'
 import './estilos.css'
 const Inicio = () => {
-    const FondoPantalla = {
-        width:'100%',
-        height:'100%',
-      };
   return (
-    <div style={FondoPantalla} className="inicio">
+    <div className="inicio">
       <div className="Barra"><NavBar/></div>
       <div className="contenedor"> 
         <div className="contenedor-item1"><CartaPerfil/></div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,10 @@
-import React,{useRef} from 'react'
+import React,{useRef, useState} from 'react'
 import './estilos.css'
 import MusicaFondo from '../../musica/musicafondo.mp3'
 import muted from '../../img/muted.png'
 import disco from '../../img/disco.gif'
-const NavBar = () => {
+import barra from '../../img/menu.png'
+function NavBar ()  {
   const audioRef = useRef(null); // Crea una referencia al elemento <audio>
   const playAudio = () => {
     audioRef.current.play() 
@@ -11,20 +12,30 @@ const NavBar = () => {
   const toggleMute = () => {
     audioRef.current.muted = !audioRef.current.muted; // Invertir el valor de muted
   };
+  const [estilos,aplicarEstilos] = useState(false);
+  const cambiarEstado=()=>{
+    aplicarEstilos(!estilos)
+  }
+  const estilo={
+    visibility:'visible'
+  }
   return (
-    <div className="Barra">  
-    <nav>
-    <audio className="Barra-audio" src= {MusicaFondo} loop ref={audioRef}/>
-    <p>clickea el disco y desmutea para escuchar musica!
-   </p>
-    <div className="Barra-Sonido">
-     <img src={disco} alt="muted" className="icons" onClick={playAudio}/>
-    <img src={muted} alt="muted" className="icons" onClick={toggleMute}/>
-    </div>
-    <a href="#home">Perfil</a>
-    <a href="#about">Contacto</a>
-    <a href="#contact">Proyectos</a>
-    <a href="#arte">Arte</a>
+    <div  className="Barra">  
+    <button className="botonBarra"  onClick={cambiarEstado}><img className="Icono" src={barra} alt="menú"/></button>
+    <nav style={estilos ? estilo:{}} >
+         
+        <audio className="Barra-audio" src= {MusicaFondo} loop ref={audioRef}/>
+        <p>clickea el disco y desmutea para escuchar musica!
+        </p>
+        <div className="Barra-Sonido">
+          <img src={disco} alt="muted" className="icons" onClick={playAudio}/>
+        <img src={muted} alt="muted" className="icons" onClick={toggleMute}/>
+        </div>
+        <a href="#home">Perfil</a>
+        <a href="#about">Contacto</a>
+        <a href="#contact">Proyectos</a>
+        <a href="#arte">Arte</a>
+        
   </nav></div>
   )
 }
